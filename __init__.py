d5259f082a54c0af0b8113ad335e4cae0121cbad
oyilmaztekin <yilmaztekin.ozer@gmail.com>
2015-12-29 11:37:21 +0200

    droplet için

--- __init__.py
+++ __init__.py
@@ -11,21 +11,21 @@ reload(sys)
 sys.setdefaultencoding("utf-8")
 
 app = Flask(__name__)
-#app.config["MONGODB_SETTINGS"] = {
-#'DB': "copylighter",
-#'host':'mongodb://localhost/copylighter',
-#'username':'copyOz',
-#'password':'7e0522bfd98d2051a20a0aca5fb3899f'
-#}
-
-#AŞAĞIDAKİ KISIM HEROKU İÇİN HAZIRLANDI
-
 app.config["MONGODB_SETTINGS"] = {
 'DB': "copylighter",
-'host':'ds037185.mongolab.com:37185/copylighter',
+'host':'mongodb://localhost/copylighter',
 'username':'copyOz',
 'password':'7e0522bfd98d2051a20a0aca5fb3899f'
 }
+
+#AŞAĞIDAKİ KISIM HEROKU İÇİN HAZIRLANDI
+
+#app.config["MONGODB_SETTINGS"] = {
+#'DB': "heroku_9d72c5vb",
+#'host':'ds037145.mongolab.com:37145/heroku_9d72c5vb',
+#'username':'copyOz',
+#'password':'7e0522bfd98d2051a20a0aca5fb3899f'
+#}
 
 app.config["SECRET_KEY"] = '6cf34ed05e241ac72456425779220bfeaf3557ef8371bed4'
 #app.config["DEBUG"] = True
@@ -33,7 +33,6 @@ app.config["SECRET_KEY"] = '6cf34ed05e241ac72456425779220bfeaf3557ef8371bed4'
 login_manager = LoginManager()
 login_manager.session_protection = "strong"
 login_manager.init_app(app)
-
 
 SECURITY_RECOVERABLE = True
 db = MongoEngine(app)
--- copylighter.wsgi
+++ copylighter.wsgi
@@ -5,3 +5,6 @@ sys.path.insert(0,"/var/www/copylighter/")
 
 from copylighter import app as application
 application.secret_key = '6cf34ed05e241ac72456425779220bfeaf3557ef8371bed4'
+
+activate_this = 'env/bin/activate_this.py'
+execfile(activate_this, dict(__file__=activate_this))
