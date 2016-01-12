3a8d5224a29e0198a47fd2d009e4e0bf388a9346
özer <yilmaztekin.ozer@gmail.com>
2016-01-12 14:38:00 +0200

    manage.py

--- manage.py
+++ manage.py
@@ -6,67 +6,11 @@ from flask.ext.script import Manager, Server
 from copylighter import app
 manager = Manager(app)
 
-from flask_script import Command, Option
-
-class GunicornServer(Command):
-
-    description = 'Run the app within Gunicorn'
-
-    def __init__(self, host='127.0.0.1', port=8000, workers=4):
-        self.port = port
-        self.host = host
-        self.workers = workers
-
-    def get_options(self):
-        return (
-            Option('-H', '--host',
-                   dest='host',
-                   default=self.host),
-
-            Option('-p', '--port',
-                   dest='port',
-                   type=int,
-                   default=self.port),
-
-            Option('-w', '--workers',
-                   dest='workers',
-                   type=int,
-                   default=self.workers),
-        )
-
-    def handle(self, app, host, port, workers):
-
-        from gunicorn import version_info
-
-        if version_info < (0, 9, 0):
-            from gunicorn.arbiter import Arbiter
-            from gunicorn.config import Config
-            arbiter = Arbiter(Config({'bind': "%s:%d" % (host, int(port)),'workers': workers}), app)
-            arbiter.run()
-        else:
-            from gunicorn.app.base import Application
-
-            class FlaskApplication(Application):
-                def init(self, parser, opts, args):
-                    return {
-                        'bind': '{0}:{1}'.format(host, port),
-                        'workers': workers 
-                    }
-
-                def load(self):
-                    return app
-
-            FlaskApplication().run()
-
 # Turn on debugger by default and reloader
 manager.add_command("runserver", Server(
     use_debugger = True,
-    use_reloader = True,
-    host = '0.0.0.0')
+    use_reloader = True)
 )
-
-manager.add_command("runserver", GunicornServer())
-
 
 if __name__ == "__main__":
     manager.run()
