87801fd74ccf0cca62465b94ceb08dca1cdfa45e
özer <yilmaztekin.ozer@gmail.com>
2016-01-12 15:32:56 +0200

    düzeltme

--- manage.py
+++ manage.py
@@ -4,12 +4,15 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from flask.ext.script import Manager, Server
 from copylighter import app
+
+
 manager = Manager(app)
 
 # Turn on debugger by default and reloader
 manager.add_command("runserver", Server(
     use_debugger = True,
-    use_reloader = True)
+    use_reloader = True,
+    host = '0.0.0.0')
 )
 
 if __name__ == "__main__":
